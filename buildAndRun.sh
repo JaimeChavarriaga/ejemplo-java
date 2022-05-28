2f5739cd2313e980d5958cf7fcb89a1f24cf97c3
JaimeChavarriaga <JaimeChavarriaga@users.noreply.github.com>
2022-05-28 17:39:52 -0500

    cambios en clase

--- buildAndRun.sh
+++ buildAndRun.sh
@@ -1,10 +1,11 @@
-#!/bin/sh
+#!/usr/bin/bash
 
 # Compila y crea la imagen para Docker
 mvn clean package && \
-    docker build -t tendencias.ejemplos/java-docker .
+    docker.exe build -t tendencias.ejemplos/java-docker .
 
 # Ejecuta el contenedor con el nombre java-docker
 # expone los puertos 8080 y 4848
-docker rm -f java-docker || true && \
-    docker run -d -p 8080:8080 -p 4848:4848 --name java-docker tendencias.ejemplos/java-docker 
+docker.exe stop java-docker || true && \
+    docker.exe rm -f java-docker || true && \
+    docker.exe run -d -p 8080:8080 -p 4848:4848 --name java-docker tendencias.ejemplos/java-docker 
